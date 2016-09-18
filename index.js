cecd6f14a4f878a6e265327a3814fa9bc12db85e
Jan-Henrik Bruhn <jh.bruhn97@gmail.com>
2016-09-18 11:58:28 +0200

    Fix image urls when archive is disabled

--- index.js
+++ index.js
@@ -153,7 +153,9 @@ var transformArticleBody = function(id, sysDate, page, storyId, body, cb) {
     var storyId = imageId.split("@")[1];
     imageId = imageId.split("@")[0];
 
-    var imageFilename = getOutputFolderPath(id) + "/images/" + imageId + ".jpg";
+    var articleUrl = archive ? url + "/" + id + "/" + article.storyId + ".html" : url + "/today/" + article.storyId + ".html"
+
+    var imageFilename = getOutputFolderPath(archive ? id : "today") + "/images/" + imageId + ".jpg";
     downloadPicture(id, sysDate, page, storyId, imageId, imageFilename, function() {
       image.prepend(function(index) {
         if(index == 0) return "<img class='image' src='" + url + "/" + id + "/images/" + imageId + ".jpg" + "' />";
